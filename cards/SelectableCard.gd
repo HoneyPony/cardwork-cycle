d6d8224d0368cebe1ab5a94f1de591143e51adf2
HoneyPony <honeyponygames@gmail.com>
2023-01-07 13:46:53 -0500

    Basic functionality of new card UI
    Former-commit-id: bc834f0226203c51763a7e3fc27e4e737e03e674

---
+++ cards/SelectableCard.gd
@@ -0,0 +1,47 @@
+extends Area2D
+
+var is_selected = false
+var is_hovered = false
+
+var associated_card
+
+# The rotation of the card while it is not hovered. Updated by the CardManager
+var in_hand_rotation = 0
+
+var in_hand_position = Vector2.ZERO
+var in_hand_hover_position = Vector2.ZERO
+
+func _ready():
+	in_hand_position = position
+	in_hand_hover_position = position
+
+func update_transform():
+	var target_scale = 1.0
+	var target_rot = in_hand_rotation
+	var target_pos = in_hand_position
+	z_index = 0
+	if is_hovered:
+		target_scale = 1.1
+		target_rot = 0
+		target_pos = in_hand_hover_position
+		z_index = 100
+		
+	if is_selected:
+		target_pos.y -= 32
+		
+	scale.x += (target_scale - scale.x) * 0.2
+	scale.y = scale.x
+	
+	rotation += (target_rot - rotation) * 0.1
+	position += (target_pos - position) * 0.15
+	
+func _physics_process(delta):
+	if is_hovered:
+		if Input.is_action_just_released("mouse"):
+			is_selected = !is_selected
+			if is_selected:
+				get_parent().get_parent().deselect_others(self)
+			else:
+				get_parent().get_parent().deselect_all()
+			
+	update_transform()
---
+++ ui/NewCardSelection.gd
@@ -0,0 +1,66 @@
+extends Control
+
+var current_card = null
+
+func _ready():
+	$ConfirmButton.disabled = true
+
+func find_card_under_mouse():
+	var space = get_world_2d().direct_space_state
+	var mouse = get_global_mouse_position()
+
+	var collisions = space.intersect_point(mouse, 32, [], 0x7FFFFFFF, false, true)
+	
+	var card = null
+	
+	for collision in collisions:
+		var obj = collision["collider"]
+		if obj.is_in_group("SelectableCard"):
+			card = obj
+			
+			# Always prefer the current card to any other card, to prevent
+			# oscillating behavior.
+			if card == current_card:
+				return card
+			
+	return card
+	
+func release_current_card():
+	if current_card == null:
+		return
+		
+	current_card.is_hovered = false
+	current_card = null
+
+func _physics_process(delta):
+	var under_mouse = find_card_under_mouse()
+		
+	if under_mouse != current_card:
+		release_current_card()
+		current_card = under_mouse
+		
+		if is_instance_valid(current_card):
+			current_card.is_hovered = true
+
+func deselect_others(selected_card):
+	$GoldButton.pressed = false
+	for card in $Cards.get_children():
+		if card != selected_card:
+			card.is_selected = false
+			
+	$ConfirmButton.disabled = false
+	
+func deselect_all():
+	$GoldButton.pressed = false
+	for card in $Cards.get_children():
+		card.is_selected = false
+		
+	# Nothing is selected -> confirm button is greyed out.
+	$ConfirmButton.disabled = true
+
+func _on_GoldButton_pressed():
+	for card in $Cards.get_children():
+		card.is_selected = false
+		
+	# If the gold button is also not pressed, we can't confirm.
+	$ConfirmButton.disabled = not $GoldButton.pressed
