53c4ec7138ae6fab0e47cbec337b3e47f4ecec6c
HoneyPony <honeyponygames@gmail.com>
2023-01-06 21:05:46 -0500

    Basic card mouse interactions
    Former-commit-id: 442ce32c72da2040f88397614d1b2656a091d1d1

--- cards/Card.gd
+++ cards/Card.gd
@@ -1,23 +1,59 @@
 extends Area2D
 
-var hovering = false
+const STATE_IN_HAND = 0
+const STATE_HOVER = 1
+const STATE_PICKUP = 2
+
+var state = STATE_IN_HAND
 
 # The rotation of the card while it is not hovered. Updated by the CardManager
 var in_hand_rotation = 0
 
-func update_scale():
+var in_hand_position = Vector2.ZERO
+var in_hand_hover_position = Vector2.ZERO
+
+func is_picked_up():
+	return state == STATE_PICKUP
+
+func release():
+	# TODO: Figure out how this will work with picked up cards
+	if state == STATE_HOVER:
+		state = STATE_IN_HAND
+		
+func hover():
+	if state == STATE_IN_HAND:
+		state = STATE_HOVER
+
+func update_transform():
 	var target_scale = 1.0
 	var target_rot = in_hand_rotation
-	if hovering:
+	var target_pos = in_hand_position
+	z_index = 0
+	if state == STATE_HOVER or state == STATE_PICKUP:
 		target_scale = 1.3
 		target_rot = 0
+		target_pos = in_hand_hover_position
+		z_index = 100
+	if state == STATE_PICKUP:
+		# Position should be locked
+		target_pos = position
+		target_scale = 0.7
 		
 	scale.x += (target_scale - scale.x) * 0.2
 	scale.y = scale.x
 	
 	rotation += (target_rot - rotation) * 0.1
+	position += (target_pos - position) * 0.15
 
 func _physics_process(delta):
-	
+	if state == STATE_PICKUP:
+		global_position = get_global_mouse_position()
 		
-	update_scale()
+		if Input.is_action_just_released("mouse"):
+			state = STATE_HOVER # TODO: Be able to play card!
+	
+	if state == STATE_HOVER:
+		if Input.is_action_just_pressed("mouse"):
+			state = STATE_PICKUP
+			
+	update_transform()
--- cards/CardManager.gd
+++ cards/CardManager.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+onready var camera = get_node("../Camera")
+
 # Nullable! Be careful
 var current_card = null
 
@@ -25,51 +27,96 @@ func find_card_under_mouse():
 	
 func release_current_card():
 	if is_instance_valid(current_card):
-		current_card.hovering = false
+		current_card.release()
 		current_card = null
 		
-func organize_cards():
+func compute_y(theta, o, s):
+	if theta < 0.001:
+		return 0
+	var k = (o / sin(theta)) - s
+	var b = sqrt((s + k) * (s + k) - o * o)
+	
+	return s + k - b
+		
+func organize_cards(card_is_picked_up):
 	var center_card_height = 300.0
-	var edge_card_height = 180.0
-	var radius = center_card_height * 4.0
 	
-	var r1 = radius - center_card_height
-	var r2 = radius - edge_card_height
-	
-	var center = Vector2(0, r1)
+	var x_step = 200
+	var angle_step = deg2rad(4)
 	
 	var num_cards = get_child_count()
+	var start = -(num_cards - 1) / 2
 	
-	var angle = acos(r1 / r2)
-	var angle_step = (angle * 2) / (num_cards - 1)
+	var y_base = -260
+	if card_is_picked_up:
+		y_base = 80
 	
-	var cur_angle = angle + PI * 0.5
-	
-	for c in get_children():
-		var p = polar2cartesian(radius, cur_angle)
-		p.y *= -1
+	for i in range(0, get_child_count()):
+		var c = get_child(i)
 		
-		c.position = center + p
-		c.in_hand_rotation = PI - (cur_angle + 0.5 * PI)
+		var x_offset = x_step * (i + start)
+		var angle_offset = angle_step * (i + start)
 		
-		cur_angle -= angle_step
+		#var y_offset = abs(x_offset) * tan(abs(angle_offset))
+		var y_offset = compute_y(abs(angle_offset), abs(x_offset), 140)
+		
+		var hand_pos = Vector2(x_offset, y_base + y_offset)
+
+		c.in_hand_position = hand_pos
+		c.in_hand_hover_position = Vector2(hand_pos.x, y_base)
+		c.in_hand_rotation = angle_offset
 	
+	
+#func update_camera():
+#	var x = camera.position.x
+#	if current_card:
+#		var dist_to_card = abs(current_card.position.x - x)
+#		var max_dist = get_viewport().size.x * 0.5 - 180
+#
+##		if dist_to_card > max_dist:	
+##			var cx = current_card.position.x
+##			var mx = get_global_mouse_position().x
+##			var move_mouse_to_card = (cx - mx)
+##
+##			camera.position.x += move_mouse_to_card * 0.1
+#
+##		var dist_to_card = abs(current_card.position.x - x)
+##
+#		#var t = dist_to_card / 10000
+#		#t = clamp(t, 0.0, 0.1)
+#		if dist_to_card > max_dist:
+#			var target_x = (current_card.position.x + get_global_mouse_position().x) * 0.5
+#			var diff = (target_x - x) * 0.03
+#			if abs(diff) > 5:
+#				diff = sign(diff) * 5
+#			x += diff
+#			camera.position.x = x
+
+func update_camera():
+	# TODO: Implement scroll-wheel-based scrolling, maybe?
+	pass
+	
+func card_is_picked_up():
+	if is_instance_valid(current_card):
+		return current_card.is_picked_up()
+	return false
 	
 func _physics_process(delta):
-	# This node is parented to the camera, so we can offset to the
-	# bottom of the camera simply by going viewport.size.y / 2
+	# This node is offset off the bottom of the camera viewport.size.y / 2
+	position.y = camera.position.y + get_viewport().size.y / 2
 	
-	position.y = get_viewport().size.y / 2
-	
-	var under_mouse = find_card_under_mouse()
-	
-	if under_mouse != current_card:
-		release_current_card()
-		current_card = under_mouse
+	var card_picked_up = card_is_picked_up()
+	if not card_picked_up:
+		var under_mouse = find_card_under_mouse()
 		
-		if is_instance_valid(current_card):
-			current_card.hovering = true
+		if under_mouse != current_card:
+			release_current_card()
+			current_card = under_mouse
 			
-	organize_cards()
+			if is_instance_valid(current_card):
+				current_card.hover()
+			
+	organize_cards(card_picked_up)
+	update_camera()
 			
 	
