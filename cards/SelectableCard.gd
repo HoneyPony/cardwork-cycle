ef09bb8954a9323445425ce0d1e8f6e6280d135b
HoneyPony <honeyponygames@gmail.com>
2023-01-07 14:17:52 -0500

    A strategy that basically makes the card UI work.
    It is very annoying that Area2D apparently does not work on top
    of CanvasLayer, AND that for some reason Godot will not render fonts
    correctly if the label isn't on a CanvasLayer (????)
    
    
    Former-commit-id: d09c45683f5045a2facaf0e65638d66bd83702fb

--- cards/SelectableCard.gd
+++ cards/SelectableCard.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Control
 
 var is_selected = false
 var is_hovered = false
@@ -11,32 +11,44 @@ var in_hand_rotation = 0
 var in_hand_position = Vector2.ZERO
 var in_hand_hover_position = Vector2.ZERO
 
+var card_child
+
 func _ready():
-	in_hand_position = position
-	in_hand_hover_position = position
+	card_child = $CardBase
+
+#func _ready():
+#	in_hand_position = 
+#	in_hand_hover_position = position
 
 func update_transform():
 	var target_scale = 1.0
 	var target_rot = in_hand_rotation
 	var target_pos = in_hand_position
-	z_index = 0
+	#z_index = 0
 	if is_hovered:
 		target_scale = 1.1
 		target_rot = 0
 		target_pos = in_hand_hover_position
-		z_index = 100
+		#z_index = 100
 		
 	if is_selected:
 		target_pos.y -= 32
 		
-	scale.x += (target_scale - scale.x) * 0.2
-	scale.y = scale.x
+	target_scale *= 0.5 # Account fo rscaling
+	target_pos += Vector2(rect_size.x * 0.5, rect_size.y * 0.5)
+		
+	card_child.scale.x += (target_scale - card_child.scale.x) * 0.2
+	card_child.scale.y = card_child.scale.x
 	
-	rotation += (target_rot - rotation) * 0.1
-	position += (target_pos - position) * 0.15
+	#rotation += (target_rot - rotation) * 0.1
+	card_child.position += (target_pos - card_child.position) * 0.15
+	
+func check_hover():
+	var m = get_local_mouse_position()
+	return m.x >= 0 && m.y >= 0 && m.x <= rect_size.x && m.y <= rect_size.y
 	
 func _physics_process(delta):
-	print(get_world_2d())
+	is_hovered = check_hover()
 	
 	if is_hovered:
 		if Input.is_action_just_released("mouse"):
--- ui/NewCardSelection.gd
+++ ui/NewCardSelection.gd
@@ -5,25 +5,25 @@ var current_card = null
 func _ready():
 	$ConfirmButton.disabled = true
 
-func find_card_under_mouse():
-	var space = get_world_2d().direct_space_state
-	var mouse = get_parent().get_local_mouse_position()
-
-	var collisions = space.intersect_point(mouse, 32, [], 0x7FFFFFFF, false, true)
-	
-	var card = null
-	
-	for collision in collisions:
-		var obj = collision["collider"]
-		if obj.is_in_group("SelectableCard"):
-			card = obj
-			
-			# Always prefer the current card to any other card, to prevent
-			# oscillating behavior.
-			if card == current_card:
-				return card
-			
-	return card
+#func find_card_under_mouse():
+#	var space = get_world_2d().direct_space_state
+#	var mouse = get_parent().get_local_mouse_position()
+#
+#	var collisions = space.intersect_point(mouse, 32, [], 0x7FFFFFFF, false, true)
+#
+#	var card = null
+#
+#	for collision in collisions:
+#		var obj = collision["collider"]
+#		if obj.is_in_group("SelectableCard"):
+#			card = obj
+#
+#			# Always prefer the current card to any other card, to prevent
+#			# oscillating behavior.
+#			if card == current_card:
+#				return card
+#
+#	return card
 	
 func release_current_card():
 	if current_card == null:
@@ -32,15 +32,15 @@ func release_current_card():
 	current_card.is_hovered = false
 	current_card = null
 
-func _physics_process(delta):
-	var under_mouse = find_card_under_mouse()
-		
-	if under_mouse != current_card:
-		release_current_card()
-		current_card = under_mouse
-		
-		if is_instance_valid(current_card):
-			current_card.is_hovered = true
+#func _physics_process(delta):
+#	var under_mouse = find_card_under_mouse()
+#
+#	if under_mouse != current_card:
+#		release_current_card()
+#		current_card = under_mouse
+#
+#		if is_instance_valid(current_card):
+#			current_card.is_hovered = true
 
 func deselect_others(selected_card):
 	$GoldButton.pressed = false
