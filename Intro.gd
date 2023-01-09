df48b259866e58ca255d0dc67483f805e6003506
HoneyPony <honeyponygames@gmail.com>
2023-01-09 12:51:24 -0500

    Intro and quieter nowater sound
    Former-commit-id: 23957f509d5e468e995fc7f372e513043970bcf6

--- Intro.gd
+++ Intro.gd
@@ -2,3 +2,8 @@ extends Control
 
 func _on_Button_pressed():
 	get_tree().change_scene_to(GS.Game)
+	
+func _process(delta):
+	if Input.is_action_just_pressed("mouse"):
+		var anim = $AnimationPlayer
+		anim.seek(anim.current_animation_length)
