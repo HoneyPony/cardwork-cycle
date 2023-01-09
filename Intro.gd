1cca82714e6f20f4f0cc0a53c9df0218285022b1
HoneyPony <honeyponygames@gmail.com>
2023-01-09 14:49:51 -0500

    Small polish on Intro
    Former-commit-id: 414cd89d0a8719b80bc9a657b84fc9c68f24b28f

--- Intro.gd
+++ Intro.gd
@@ -3,6 +3,10 @@ extends Control
 func _on_Button_pressed():
 	SceneTransitions.change_scene_to(GS.Game)
 	
+func _ready():
+	if Flags.skip_tutorial:
+		$L4.text = "Looking around your new farm, you spot a peculiar looking rock. Hidden under it you find a dusty scroll, and a dustier deck of cards."
+	
 func _process(delta):
 	if Input.is_action_just_pressed("mouse"):
 		var anim = $AnimationPlayer
